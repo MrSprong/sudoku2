351569a304a40e7862c3c48a4fd59f8dce9dbc69
MrSprong <50913529+MrSprong@users.noreply.github.com>
2020-03-10 11:16:19 +0100

    Meneer Spth eerst commit

---
+++ SudokuTspr19feb2020/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SudokuTspr19feb2020
+{
+    public partial class Form1 : Form
+    {
+        int rows = 0;
+        int horizontalBlocks = 3;
+        int verticalBlocks = 3;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Width = 480;
+            Height = 700;
+            for (int k = 0; k < verticalBlocks; k++)
+            {
+                for (int j = 0; j < horizontalBlocks; j++)
+                {
+                    for (int length = 0; length < 9; length++)
+                    {
+                        for (int width = 0; width < 9; width++)
+                        {
+                            TextBox t = new TextBox();
+                            t.Width = 15;
+                            t.Height = 15;
+                            this.Controls.Add(t);
+                            t.Left = (width * 16) + (j * 150);
+                            t.Top = (rows * 20) + (k*200);
+                        }
+                        rows++;
+                    }
+                    rows = 0;
+                }
+            }
+        }
+    }
+}
